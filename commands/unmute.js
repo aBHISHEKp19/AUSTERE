4d83f933d5a2ea92956faafc682fb07ebd410c98
aBHISHEKp <abhishekpoddar4776@gmail.com>
2021-12-07 19:34:41 +0530

    unmute
    ewfwwf

--- commands/unmute.js
+++ commands/unmute.js
@@ -21,7 +21,7 @@ module.exports = {
 
                 memberTarget.roles.remove(muteRole.id);
                 memberTarget.roles.add(mainRole.id);
-                message.channel.send(`\`<@${memberTarget.user.id}> has been umuted\``);
+                message.channel.send(`<@${memberTarget.user.id}> has been umuted`);
 
             }
         }

c8eced774d0c1b98c18c13fd8d75ccbfac2877b1
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-01-05 22:47:42 +0530

    c

--- commands/reactionrole.js
+++ commands/reactionrole.js
@@ -1,15 +1,33 @@
-module.exports = {
+/* module.exports = {
     name: 'reactionrole',
-    description: 'reactions',
+        description: 'reactions',
 
-    execute(message, args, client, Discord) {
+            execute(message, args, client, Discord) {
 
         const channel = '928278615181897748';
         const appyrole = message.guild.roles.cache.find(role => role.name === "Appy");
         const milkyrole = message.guild.roles.cache.find(role => role.name === "Milky");
+        const appyemoji = ':blue_heart:';
+        const milkyemoji = ':heart:';
+
+        let roleembed = new Discord.MessageEmbed()
+            .setColor('#e42643')
+            .setTitle('React to get your role')
+            .setDescription('Choose any role you wish for amongst the below ones \n\n'
+                + `${appyemoji} for appy role \n`
+                + `${milkyemoji} for milky role`);
+   
+
+        let MessageEmbed = await message.channel.send({ embeds: [roleembed] });
+        MessageEmbed.react(appyemoji);
+        MessageEmbed.react(milkyemoji);
+
+        client.on('messageReactionAdd', async (reaction, user) =>  {
+
+
+        })
+
 
 
     }
-
-
-}
+}*/
--- main.js
+++ main.js
@@ -12,15 +12,17 @@ const bot = new Client({
 
 
 
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] }); //alt
+//const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] }); //alt
 
 const { MessageEmbed } = require('discord.js');
 
 require('dotenv').config();
 
 
-const client = new Client({
+const client = new Discord.Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING],
+    partials: ["MESSAGE", "CHANNEL", "REACTION"]
+
 });
 
 

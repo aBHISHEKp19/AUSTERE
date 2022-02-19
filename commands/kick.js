136d2cf6d790ae85fa24c3a71f18c958e5cf1b0c
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-02-19 17:37:27 +0530

    f

--- commands/kick.js
+++ commands/kick.js
@@ -13,7 +13,7 @@ module.exports = {
 
             .setColor('#40E0D0')
             .setTitle("Aapko kick krdu? <:whatislife:844660332978765854>")
-    
+
 
         if (message.member.permissions.has("ADMINISTRATOR")) {
 
@@ -29,7 +29,7 @@ module.exports = {
 
 
                 //.setURL 
- 
+
                 //.addField("Tag", `${member.userTag}`)
                 .addField("ID Created:", `${membertarger.user.createdAt}`)
                 .addField("Joined the server on:", `${membertarger.joinedAt}`)
@@ -38,17 +38,15 @@ module.exports = {
                 //.addField("User Age", `${membertarger.userAge}`)
                 //.addField("User ID:", `${membertarger.id}`)
                 .setThumbnail(membertarger.displayAvatarURL({ dynamic: true }))
-                .addField("User Age:", membertarger.userAge)
+              //  .addField("User Age:", `${membertarger.user.userAge}`)
                 //.addField("Moderator", `${author.username}`)
 
                 //.setImage('https://i.pinimg.com/550x/9c/4b/08/9c4b08ecfeb8bb750e89dfba3e0aa08b.jpg')
 
-                .setFooter(`User ID: ${membertarger.id}`)("Moderator:", `${author.username}`)
+                .setFooter(`User ID: ${membertarger.id} \nPunisher: ${message.author.tag}`)
                 .setTimestamp();
 
             message.channel.send({ embeds: [newEmbed] });
-
-
             membertarger.kick();
             // message.channel.send(`kick kardiya <@${membertarger.user.id}> ko`);
         }
@@ -56,7 +54,6 @@ module.exports = {
 
 
         else {
-
 
             message.channel.send({ embeds: [uhhun] });
 
